7b71ee64e5bfb5d4ac6d7ec3e284ee9370244a47
David Retamal <d.retamal@alumnos.urjc.es>
2015-04-12 21:36:28 +0200

    Historia hecho :dancers:
    ea

--- Practica Final/js/script.js
+++ Practica Final/js/script.js
@@ -13,7 +13,7 @@ $(function(){
 
                  var targetOffset = $target.offset().top;
 
-                 $('html,body').animate({scrollTop: targetOffset}, 1000);
+                 $('html,body').animate({scrollTop: targetOffset}, 500);
 
                  return false;
 
